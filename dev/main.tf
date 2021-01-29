5ec3b4387103cfce2628b24d2071e62a241768f0
johnelauria <john@fastfwd.com>
2021-01-29 11:35:04 +0800

    Forgot to add CIDR blocks in Security group

--- dev/main.tf
+++ dev/main.tf
@@ -19,15 +19,17 @@ resource "aws_instance" "terraform_sample_ec2" {
 
 resource "aws_security_group" "terraform_sample_sg" {
   ingress {
-    from_port = 80
-    protocol  = "TCP"
-    to_port   = 80
+    from_port   = 80
+    protocol    = "TCP"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    protocol  = "-1"
-    to_port   = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
